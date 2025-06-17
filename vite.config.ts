3168f36c35dc7b2ad511b6248b18acee24c303f9
MaximProger <maksimkoshman090@gmail.com>
2025-06-17 12:37:20 +0300

    feat: добавил pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,38 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import flowbiteReact from 'flowbite-react/plugin/vite';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    flowbiteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Дневник катастрофизации',
+        short_name: 'Дневник катастрофизации',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/favicon/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
